fb6356072f70de4d9e26688da0457a1472185cd9
FilipaMerino <filipaam@outlook.com>
2023-08-07 19:38:37 +0100

    firebase

---
+++ app/utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAe6bcKsumhpYY03NEPw6ULLXRfxiAHbnk",
+  authDomain: "virtuosaapp-7b48d.firebaseapp.com",
+  projectId: "virtuosaapp-7b48d",
+  storageBucket: "virtuosaapp-7b48d.appspot.com",
+  messagingSenderId: "885395260019",
+  appId: "1:885395260019:web:67bf041086b2d6a3a72006"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
