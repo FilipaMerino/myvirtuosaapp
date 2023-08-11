e4960817065815b95d4af08990d5e7f0815b34e0
FilipaMerino <filipaam@outlook.com>
2023-08-11 20:33:39 +0100

    meta tag

--- app/page.jsx
+++ app/page.jsx
@@ -12,6 +12,11 @@ import blogBackSquare from "../public/images/blogBackSquare.png";
 
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+export const metadata = {
+  title: "Admin Virtuosa",
+  description: "Online Business Management and Virtual Assistant services customized to meet your unique business needs. We help businesses grow sustainably through strategic business planning and Virtual Assistant support. Founded and led by an experienced Online Business Manager who has personally faced burnout, we understand the challenges businesses and individuals encounter. Our handpicked assistants prioritize your unique needs with a compassionate approach, ensuring you achieve success.",
+};
+
 
 const HomePage = () => {
   return (
