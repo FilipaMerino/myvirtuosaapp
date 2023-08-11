16a9f0bbb87c46d3cdf6efd0210dc2cb091b9533
FilipaMerino <filipaam@outlook.com>
2023-08-11 20:26:31 +0100

    description

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Admin Virtuosa",
-  description: "This is a business management website, that offers services of web development, virtual assistance and business management",
+  description: "Online Business Management and Virtual Assistant services customized to meet your unique business needs. We help businesses grow sustainably through strategic business planning and Virtual Assistant support. Founded and led by an experienced Online Business Manager who has personally faced burnout, we understand the challenges businesses and individuals encounter. Our handpicked assistants prioritize your unique needs with a compassionate approach, ensuring you achieve success.",
 };
 
 export default function RootLayout({ children }) {
