de362ec71aa44e8f701a6f5eee5e6332aae0b114
FilipaMerino <filipaam@outlook.com>
2023-10-02 16:46:24 +0100

    Changes in Logo - navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,22 +1,26 @@
-'use client'
+"use client";
 
-import Link from 'next/link'
-import { FiLogOut } from 'react-icons/fi'
-import { RxHamburgerMenu } from 'react-icons/rx'
-import { useLogout } from '../hooks/auth'
-import { useAuthState } from 'react-firebase-hooks/auth'
-import { auth } from '../lib/firebase'
+import Link from "next/link";
+import { FiLogOut } from "react-icons/fi";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { useLogout } from "../hooks/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../lib/firebase";
 // import { useContext } from "react";
 // import { UserContext } from "../contexts/UserContext";
 // import { signOutUser } from "../../utils/firebase/firebase.utils"
+import logoNavbar from "../../public/images/logoNavBar.png";
+import logoNavbarOrange from "../../public/images/logoNavBarOrange.png";
+
+import Image from "next/image";
 
 const Navbar = () => {
-  const { logout, isLoading } = useLogout()
-  const [user, loading, error] = useAuthState(auth)
+  const { logout, isLoading } = useLogout();
+  const [user, loading, error] = useAuthState(auth);
 
   return (
     <div>
-      <nav className="bg-[#333333] w-full pr-5">
+      <nav className="bg-[#333333] w-full pr-5 h-[10rem] flex items-center">
         <div className="navbar lg:flex lg:justify-between">
           <div className="navbar-start">
             <div className="dropdown">
@@ -28,8 +32,7 @@ const Navbar = () => {
 
               <ul
                 tabIndex={0}
-                className="bg-white menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow rounded-box w-32"
-              >
+                className="bg-white menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow rounded-box w-32">
                 <li>
                   <Link href="/about">About</Link>
                 </li>
@@ -51,44 +54,60 @@ const Navbar = () => {
               </ul>
             </div>
 
-            <Link
-              className="btn btn-ghost normal-case lg:text-lg text-md text-white"
-              href="/"
-            >
-              ADMIN VIRTUOSA
-            </Link>
+            <div className="flex items-center">
+              <Link href="/">
+                <Image
+                  src={logoNavbarOrange}
+                  height={250}
+                  width={250}
+                  alt="logo"
+                  className=""
+                />
+              </Link>
+
+              {/* <Link
+                className="btn btn-ghost normal-case lg:text-lg text-md text-white lg:ml-[-20px]"
+                href="/">
+                ADMIN VIRTUOSA
+              </Link> */}
+            </div>
           </div>
 
           <div className="navbar-center hidden lg:flex lg:justify-between lg:items-center gap-6">
+            <Link
+              href="/about"
+              className="link hover:text-[#f2f2ef5e] no-underline text-[15px] text-white ">
+              About
+            </Link>
 
+            <Link
+              href="/#service"
+              className="link no-underline hover:text-[#f2f2ef5e] text-[15px] text-white ">
+              Services
+            </Link>
 
-                <Link href="/about" className='link hover:text-[#f2f2ef5e] no-underline text-[14px] text-white'>About</Link>
+            <Link
+              href="/blog"
+              className="link no-underline hover:text-[#f2f2ef5e] text-[15px] text-white ">
+              Blog
+            </Link>
 
+            {user && (
+              <button type="button" onClick={logout}>
+                <Link
+                  href="/"
+                  className="link no-underline hover:text-[#f2f2ef5e] text-[14px] text-white">
+                  <FiLogOut />
+                </Link>
+              </button>
+            )}
 
-
-                <Link href="/#service" className='link no-underline hover:text-[#f2f2ef5e] text-[14px] text-white'>Services</Link>
-
-
-                <Link href="/blog" className='link no-underline hover:text-[#f2f2ef5e] text-[14px] text-white'>Blog</Link>
-
-
-                {user && (
-                  <button type="button" onClick={logout}>
-                    <Link href="/"
-                    className='link no-underline hover:text-[#f2f2ef5e] text-[14px] text-white'>
-                      <FiLogOut />
-                    </Link>
-                  </button>
-                )}
-
-                {/* )} */}
-
-
+            {/* )} */}
           </div>
         </div>
       </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
